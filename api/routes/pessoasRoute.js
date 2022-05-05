ebd3cdf50875741e81ce1f4f7e386f387bf59a50
Rafael Alcântara <devrafasouza@gmail.com>
2022-05-05 09:22:57 -0300

    implementação dos models: 'matriculas, niveis, turmas' e suas associações

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,9 @@ const pessoaController = require('../controllers/PessoaController.js');
 const router = Router();
 
 router.get('/pessoas', pessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', pessoaController.pegaUmaPessoa);
+router.post('/pessoas/', pessoaController.criaPessoa);
+router.put('/pessoas/:id', pessoaController.atualizaPessoa);
+router.delete('/pessoas/:id', pessoaController.removePessoa);
 
 module.exports = router;
